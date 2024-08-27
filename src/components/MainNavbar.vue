39e4b9ad50b9a9ab7b4ac210e763e814034f9532
Your Name <you@example.com>
2024-08-28 03:08:48 +0330

    updated

---
+++ src/components/MainNavbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body fixed-top px-3 border-bottom border-3">
+    <a class="navbar-brand" href="/">Utilities & Tools</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Offcanvas -->
+    <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="navbarOffcanvasLg">
+      <!-- Offcanvas header -->
+      <div class="offcanvas-header">
+        <div class="offcanvas-title" id="offcanvasNavbarLabel">Utilities &
+          Tools</div>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+      </div>
+      <!-- offcanvas body -->
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 d-flex align-items-center">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <ThemeSwitch />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+import ThemeSwitch from './ThemeSwitch.vue';
+
+export default {
+  name: 'MainNavbar',
+  components: {
+    ThemeSwitch,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/ThemeSwitch.vue
@@ -0,0 +1,43 @@
+<!-- src/components/ThemeSwitch.vue -->
+<template>
+  <div class="form-check form-switch mx-3">
+    <label class="form-check-label" for="theme-switch">
+      <i class="bi bi-sun-fill mx-1"></i>
+      <i class="bi bi-moon-stars-fill mx-1"></i>
+    </label>
+    <input class="form-check-input" type="checkbox" id="theme-switch" />
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const themeSwitch = document.querySelector('.form-check-input');
+    const body = document.body;
+
+    // Load the theme state from localStorage
+    const storedGlobalTheme = localStorage.getItem('globalTheme');
+    if (storedGlobalTheme) {
+      body.setAttribute('data-bs-theme', storedGlobalTheme);
+      if (storedGlobalTheme === 'dark') {
+        themeSwitch.checked = true;
+      }
+    } else {
+      body.setAttribute('data-bs-theme', 'dark');
+      localStorage.setItem('globalTheme', 'dark');
+      themeSwitch.checked = true;
+    }
+
+    // Save the theme state to localStorage when the switch is changed
+    themeSwitch.addEventListener('change', () => {
+      const newTheme = themeSwitch.checked ? 'dark' : 'light';
+      body.setAttribute('data-bs-theme', newTheme);
+      localStorage.setItem('globalTheme', newTheme);
+    });
+  }
+};
+</script>
+
+<style scoped>
+/* Add any specific styles for the theme switch here if needed */
+</style>
